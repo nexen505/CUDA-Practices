e59c98b35eb4a2bdbcaf099c1b399b175e72931a
nexen505 <nexen505@yandex.ru>
2018-11-05 21:30:51 +0400

    Basic implementation of bilinear interpolation by scikit and manual code.

---
+++ bilinear interpolation/bilinear_interpolation.py
@@ -0,0 +1,56 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import imageio
+import skimage
+
+
+def get_bilinear_pixel(imArr, posX, posY):
+    out = []
+
+    # Get integer and fractional parts of numbers
+    modXi = int(posX)
+    modYi = int(posY)
+    modXf = posX - modXi
+    modYf = posY - modYi
+    modXiPlusOneLim = min(modXi + 1, imArr.shape[1] - 1)
+    modYiPlusOneLim = min(modYi + 1, imArr.shape[0] - 1)
+
+    # Get pixels in four corners
+    for chan in range(imArr.shape[2]):
+        bl = imArr[modYi, modXi, chan]
+        br = imArr[modYi, modXiPlusOneLim, chan]
+        tl = imArr[modYiPlusOneLim, modXi, chan]
+        tr = imArr[modYiPlusOneLim, modXiPlusOneLim, chan]
+
+        # Calculate interpolation
+        b = modXf * br + (1. - modXf) * bl
+        t = modXf * tr + (1. - modXf) * tl
+        pxf = modYf * t + (1. - modYf) * b
+        out.append(int(pxf + 0.5))
+
+    return out
+
+
+if __name__ == "__main__":
+    im = imageio.imread("lozman_16_0.png")
+    enlargedShape = list(map(int, [im.shape[0] * 2, im.shape[1] * 2, im.shape[2]]))
+    enlargedImg = np.empty(enlargedShape, dtype=np.uint8)
+    rowScale = float(im.shape[0]) / float(enlargedImg.shape[0])
+    colScale = float(im.shape[1]) / float(enlargedImg.shape[1])
+
+    # original image
+    plt.imshow(im)
+    plt.show()
+
+    # resized by scikit image
+    plt.imshow(skimage.transform.resize(im, tuple(enlargedShape)))
+    plt.show()
+
+    # image interpolated manually
+    for r in range(enlargedImg.shape[0]):
+        for c in range(enlargedImg.shape[1]):
+            orir = r * rowScale  # Find position in original image
+            oric = c * colScale
+            enlargedImg[r, c] = get_bilinear_pixel(im, oric, orir)
+    plt.imshow(enlargedImg)
+    plt.show()
