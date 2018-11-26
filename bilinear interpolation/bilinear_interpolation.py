1467838d4d8165dd126f7ada4abd7c8ab47987e7
Ilia Komarov <nexen505@yandex.ru>
2018-11-26 17:06:59 +0400

    1) Bilinear interpolation cosmetic changes. 2) Commited missing kernel...

--- bilinear interpolation/bilinear_interpolation.py
+++ bilinear interpolation/bilinear_interpolation.py
@@ -1,36 +1,35 @@
 import os
 import time
-
 import matplotlib.pyplot as plt
 import numpy as np
 import imageio
-import pycuda.autoinit
 import pycuda.driver as cuda
-
+import pycuda.gpuarray as gpuarray
+from pycuda.tools import clear_context_caches, make_default_context
 from pycuda.compiler import SourceModule
 
 
 def get_bilinear_pixel(image: np.ndarray, x: int, y: int, scale: int = 2) -> list:
     out = []
+
     position_x = x / scale
     position_y = y / scale
-
-    modXi = int(position_x)
-    modYi = int(position_y)
-    modXf = position_x - modXi
-    modYf = position_y - modYi
-    modXiPlusOneLim = 0 if modXi + 1 >= image.shape[0] else modXi + 1
-    modYiPlusOneLim = 0 if modYi + 1 >= image.shape[1] else modYi + 1
+    mod_xi = int(position_x)
+    mod_yi = int(position_y)
+    mod_xf = position_x - mod_xi
+    mod_yf = position_y - mod_yi
+    mod_xi_plus_one_lim = 0 if mod_xi + 1 >= image.shape[0] else mod_xi + 1
+    mod_yi_plus_one_lim = 0 if mod_yi + 1 >= image.shape[1] else mod_yi + 1
 
     for chan in range(image.shape[2]):
-        bl = image[modXi, modYi, chan]
-        br = image[modXiPlusOneLim, modYi, chan]
-        tl = image[modXi, modYiPlusOneLim, chan]
-        tr = image[modXiPlusOneLim, modYiPlusOneLim, chan]
+        bl = image[mod_xi, mod_yi, chan]
+        br = image[mod_xi_plus_one_lim, mod_yi, chan]
+        tl = image[mod_xi, mod_yi_plus_one_lim, chan]
+        tr = image[mod_xi_plus_one_lim, mod_yi_plus_one_lim, chan]
 
-        b = (1.0 - modXf) * bl + modXf * br
-        t = (1.0 - modXf) * tl + modXf * tr
-        pxf = (1.0 - modYf) * b + modYf * t
+        b = (1.0 - mod_xf) * bl + mod_xf * br
+        t = (1.0 - mod_xf) * tl + mod_xf * tr
+        pxf = (1.0 - mod_yf) * b + mod_yf * t
         out.append(int(pxf))
 
     return out
@@ -52,74 +51,92 @@ def interpolate_image_manually(image: np.ndarray, scale: int = 2):
 
 
 def interpolate_image_by_cuda(image: np.ndarray, scale: int = 2):
-    start = time.time()
-    cu_module = SourceModule(open("kernel.cu", "r").read())
+    cuda.init()
+    global context
+    context = make_default_context()
+    context.get_device()
 
+    cu_module = SourceModule(open("kernel.cu", "r").read())
     interpolate = cu_module.get_function("interpolate")
 
+    start = time.time()
+
+    uint32_image = np.zeros((image.shape[0], image.shape[1]), dtype=np.uint32)
+    for x in range(uint32_image.shape[0]):
+        for y in range(uint32_image.shape[1]):
+            for ch in range(image.shape[2]):
+                uint32_image[x, y] += image[x, y, ch] << (8 * (image.shape[2] - ch - 1))
     cu_tex = cu_module.get_texref("tex")
     cu_tex.set_filter_mode(cuda.filter_mode.POINT)
     cu_tex.set_address_mode(0, cuda.address_mode.CLAMP)
     cu_tex.set_address_mode(1, cuda.address_mode.CLAMP)
+    cuda.matrix_to_texref(uint32_image, cu_tex, order="C")
 
     enlarged_image_shape = get_image_enlarged_shape(image, scale)
-
-    rgba_image = np.zeros((image.shape[0], image.shape[1]), dtype=np.uint32)
-    for x in range(rgba_image.shape[0]):
-        for y in range(rgba_image.shape[1]):
-            for ch in range(image.shape[2]):
-                rgba_image[x, y] += image[x, y, ch] << (8 * (image.shape[2] - ch - 1))
-    cuda.matrix_to_texref(rgba_image, cu_tex, order="C")
-
-    output = np.zeros((enlarged_image_shape[0], enlarged_image_shape[1]), dtype=np.uint32)
+    result = gpuarray.to_gpu(np.zeros((enlarged_image_shape[0], enlarged_image_shape[1]), dtype=np.uint32))
     block = (np.int(16), np.int(16), 1)
     grid = (
         int(np.ceil(enlarged_image_shape[0] / block[0])),
         int(np.ceil(enlarged_image_shape[1] / block[1]))
     )
 
-    interpolate(cuda.Out(output),
+    interpolate(result.gpudata,
                 np.int32(image.shape[1]),
                 np.int32(image.shape[0]),
                 np.int32(enlarged_image_shape[1]),
                 np.int32(enlarged_image_shape[0]),
+                np.int32(image.shape[2]),
                 block=block,
                 grid=grid,
                 texrefs=[cu_tex])
 
-    output_image = np.zeros((enlarged_image_shape[0], enlarged_image_shape[1], 4), dtype=np.uint32)
-    for x in range(output_image.shape[0]):
-        for y in range(output_image.shape[1]):
-            for ch in range(output_image.shape[2]):
-                output_image[x, y, output_image.shape[2] - ch - 1] = output[x, y] % 256
-                output[x, y] //= 256
+    output = result.get()
+    result.gpudata.free()
+    rgba_image = np.zeros((enlarged_image_shape[0], enlarged_image_shape[1], image.shape[2]), dtype=np.uint32)
+    for x in range(rgba_image.shape[0]):
+        for y in range(rgba_image.shape[1]):
+            output_x_y = output[x, y]
+            for ch in range(rgba_image.shape[2]):
+                rgba_image[x, y, rgba_image.shape[2] - ch - 1] = output_x_y % 256
+                output_x_y >>= 8
 
     print(f'interpolate_image_by_cuda - calculation time: {time.time() - start:.5f} s')
-    return output_image
+
+    del output
+    del uint32_image
+    context.pop()
+    context = None
+    clear_context_caches()
+
+    return rgba_image
 
 
-def show_image(image, title: str = ''):
+def show_image(image: np.ndarray, title: str = ''):
     plt.imshow(image)
     plt.title(title)
     plt.show()
 
 
-def calculate_image(image: np.ndarray, scale: int = 2):
+def calculate_image(file: str, scale: int = 2):
+    print(f'Image {file} - calculation started..')
+    image = imageio.imread(file)
     image_by_cuda = interpolate_image_by_cuda(image, scale)
     image_interpolated_manually = interpolate_image_manually(image, scale)
     np.testing.assert_array_equal(image_by_cuda, image_interpolated_manually)
     show_image(image, 'Original image')
     show_image(image_by_cuda, 'CUDA interpolation')
     show_image(image_interpolated_manually, 'Manual interpolation')
+    print(f'Image {file} - calculation finished!')
 
 
-def get_images(folder: str = './data'):
-    images = []
-    for root, dirs, files in os.walk(folder):
-        images += [imageio.imread(f'{folder}/{file}') for file in files if file.endswith(".png")]
-    return images
+def get_images_files(folder: str = './data'):
+    images_files = []
+    for ignoredRoot, ignoredDirs, files in os.walk(folder):
+        images_files += [f'{folder}/{file}' for file in files if file.endswith(".png")]
+    return images_files
 
 
 if __name__ == "__main__":
-    for img in get_images():
-        calculate_image(img)
+    folder = './data'
+    for file in get_images_files(folder):
+        calculate_image(file)
