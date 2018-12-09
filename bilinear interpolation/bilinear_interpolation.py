eb1e9bd60f3b8a912815c964e18680c4abcb6ee4
Ilia Komarov <nexen505@yandex.ru>
2018-12-09 19:18:54 +0400

    Added several test images.

--- bilinear interpolation/bilinear_interpolation.py
+++ bilinear interpolation/bilinear_interpolation.py
@@ -5,7 +5,6 @@ import numpy as np
 import imageio
 import pycuda.driver as cuda
 import pycuda.gpuarray as gpuarray
-from pycuda.tools import clear_context_caches, make_default_context
 from pycuda.compiler import SourceModule
 
 
@@ -41,46 +40,53 @@ def get_image_enlarged_shape(image: np.ndarray, scale: int = 2) -> list:
 
 def interpolate_image_manually(image: np.ndarray, scale: int = 2):
     start = time.time()
+
+    print('Getting image enlarged shape..')
     enlarged_image_shape = get_image_enlarged_shape(image, scale)
     enlarged_image = np.empty(enlarged_image_shape, dtype=np.uint8)
+
+    print('Calculating bilinear pixels..')
     for x in range(enlarged_image.shape[0]):
         for y in range(enlarged_image.shape[1]):
             enlarged_image[x, y] = get_bilinear_pixel(image, x, y, scale)
+
     print(f'interpolate_image_manually - calculation time: {time.time() - start:.5f} s')
     return enlarged_image
 
 
 def interpolate_image_by_cuda(image: np.ndarray, scale: int = 2):
-    cuda.init()
-    global context
-    context = make_default_context()
-    context.get_device()
+    import pycuda.autoinit
 
     cu_module = SourceModule(open("kernel.cu", "r").read())
     interpolate = cu_module.get_function("interpolate")
 
     start = time.time()
 
+    print('Getting color channels..')
     uint32_image = np.zeros((image.shape[0], image.shape[1]), dtype=np.uint32)
     for x in range(uint32_image.shape[0]):
         for y in range(uint32_image.shape[1]):
             for ch in range(image.shape[2]):
                 uint32_image[x, y] += image[x, y, ch] << (8 * (image.shape[2] - ch - 1))
+
+    print('Copying texture..')
     cu_tex = cu_module.get_texref("tex")
     cu_tex.set_filter_mode(cuda.filter_mode.POINT)
     cu_tex.set_address_mode(0, cuda.address_mode.CLAMP)
     cu_tex.set_address_mode(1, cuda.address_mode.CLAMP)
     cuda.matrix_to_texref(uint32_image, cu_tex, order="C")
 
+    print('Getting image enlarged shape..')
     enlarged_image_shape = get_image_enlarged_shape(image, scale)
-    result = gpuarray.to_gpu(np.zeros((enlarged_image_shape[0], enlarged_image_shape[1]), dtype=np.uint32))
-    block = (np.int(16), np.int(16), 1)
+    result = np.zeros((enlarged_image_shape[0], enlarged_image_shape[1]), dtype=np.uint32)
+    block = (16, 16, 1)
     grid = (
         int(np.ceil(enlarged_image_shape[0] / block[0])),
         int(np.ceil(enlarged_image_shape[1] / block[1]))
     )
 
-    interpolate(result.gpudata,
+    print('Interpolating..')
+    interpolate(cuda.Out(result),
                 np.int32(image.shape[1]),
                 np.int32(image.shape[0]),
                 np.int32(enlarged_image_shape[1]),
@@ -90,24 +96,19 @@ def interpolate_image_by_cuda(image: np.ndarray, scale: int = 2):
                 grid=grid,
                 texrefs=[cu_tex])
 
-    output = result.get()
-    result.gpudata.free()
+    print('Combining channels into color points..')
     rgba_image = np.zeros((enlarged_image_shape[0], enlarged_image_shape[1], image.shape[2]), dtype=np.uint32)
     for x in range(rgba_image.shape[0]):
         for y in range(rgba_image.shape[1]):
-            output_x_y = output[x, y]
+            output_x_y = result[x, y]
             for ch in range(rgba_image.shape[2]):
                 rgba_image[x, y, rgba_image.shape[2] - ch - 1] = output_x_y % 256
                 output_x_y >>= 8
 
-    print(f'interpolate_image_by_cuda - calculation time: {time.time() - start:.5f} s')
-
-    del output
+    print('Clearing temporaries..')
+    del result
     del uint32_image
-    context.pop()
-    context = None
-    clear_context_caches()
-
+    print(f'interpolate_image_by_cuda - calculation time: {time.time() - start:.5f} s')
     return rgba_image
 
 
@@ -122,17 +123,24 @@ def calculate_image(file: str, scale: int = 2):
     image = imageio.imread(file)
     image_by_cuda = interpolate_image_by_cuda(image, scale)
     image_interpolated_manually = interpolate_image_manually(image, scale)
+    print('Comparing results..')
     np.testing.assert_array_equal(image_by_cuda, image_interpolated_manually)
+    print('Showing images..')
     show_image(image, 'Original image')
     show_image(image_by_cuda, 'CUDA interpolation')
     show_image(image_interpolated_manually, 'Manual interpolation')
     print(f'Image {file} - calculation finished!')
 
 
-def get_images_files(folder: str = './data'):
+def get_images_files(folder: str = './data') -> list:
+    # images_files = [
+    #     f'{folder}/ezh.png'
+    # ]
+
     images_files = []
     for ignoredRoot, ignoredDirs, files in os.walk(folder):
-        images_files += [f'{folder}/{file}' for file in files if file.endswith(".png")]
+        images_files += [f'{folder}/{f}' for f in files if f.endswith(".png") and not f.startswith("ignored")]
+
     return images_files
 
 
